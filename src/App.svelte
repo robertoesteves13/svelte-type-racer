70755aa0bc6b9d605a2439856bbd8b338d446968
Roberto Esteves <robertoesteves@protonmail.com>
2021-06-11 20:25:50 -0300

    Add styling to page!

--- src/App.svelte
+++ src/App.svelte
@@ -1,50 +1,65 @@
 <script>
-    import KeyboardInput from './KeyboardInput.svelte';
-    let sentence = 'I am so pro at typing I can do this with ease!';
-    let sentenceWords = sentence.split(' ');
+    import KeyboardInput from "./KeyboardInput.svelte";
+    export let sentence;
+    let sentenceWords = sentence.split(" ");
+    let sentenceWordsCorrect = [];
+
     for (let i = 0; i < sentenceWords.length - 1; i++) {
-        sentenceWords[i] = sentenceWords[i] + ' ';
+        sentenceWords[i] = sentenceWords[i] + " ";
     }
 
-    console.log(sentenceWords);
-
-    let lastWord = sentenceWords.slice(-1);
     $: currentWord = sentenceWords[0];
 
 
-    function validateWord(event){
-        if (event.target.value === currentWord){
+    function validateWord(event) {
+        if (event.target.value === currentWord) {
             let first;
-            [first, ...sentenceWords] = [...sentenceWords] ;
-            sentence = sentenceWords.join(' ');
-            event.target.value = '';
-            if (!sentence) alert('You won!');
-            console.log(sentenceWords);
+            [first, ...sentenceWords] = [...sentenceWords];
+            sentenceWordsCorrect = [...sentenceWordsCorrect, first];
+            event.target.value = "";
+
+            if (sentenceWords.length == 0) {
+                event.target.disabled = true;
+                event.target.value = "You won!";
+            }
         }
     }
 </script>
 
-<h1>
-    {sentence}
-</h1>
-<input type="text" on:input={validateWord}>
+<div class="container">
+    <div class="content">
+        <label for="wordinput" class="column">
+            <span class="green">{sentenceWordsCorrect.join(" ")}</span>
+            {sentenceWords.join(" ")}
+        </label>
+        <input type="text" id="wordinput" class="column wordinput" on:input={validateWord} autofocus/>
+    </div>
+</div>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+    .green {
+        color: green;
+    }
 
-	h1 {
-		font-size: 3em;
-		font-weight: 100;
-	}
+    .wordinput {
+        border: none;
+        font-size: 2em;
+        text-align: center;
+    }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+    .wordinput:focus {
+        border: none;
+        -moz-user-select: none;
+        user-select: none;
+        outline: none;
+    }
+
+    .wordinput:disabled {
+        background-color: white;
+    }
+
+    label {
+        margin: 0 5em;
+    }
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,18 @@
 import App from './App.svelte';
 
+let sentences = [`Alfred Hitchcock once said, "Drama is life with the dull bits cut out." If he'd been alive during ancient times, he might have uttered these same words when facing a play from Euripides. Reflecting the interesting (and often) tragic parts of "life" on the stage was an important part of the ancient world, and helped to shape our modern culture.`,
+		 `Sitting back and doing nothing, or worse, criticizing others, does not require courage. Speaking up and letting your unique voice be heard is an important step for anyone who wants to make anything better.`,
+		 `What good's a reward if you ain't around to use it? Besides, attacking that battle station ain't my idea of courage. It's more like... suicide.`,
+		 `If you're committed enough, you can make any story work. I once convinced a woman that I was Kevin Costner, and it worked because I believed it.`,
+		 `When is the last time you cared about something except yourself, hot rod? You name me one time and I will take it all back. I didn't think so. These are good folk around here, who care about one another. I don't want 'em depending on someone they can't count on.`]
+
+let randomNumber = Math.floor(Math.random() * sentences.length);
+let sentence = sentences[randomNumber];
+
 const app = new App({
 	target: document.body,
 	props: {
-		name: 'world'
+		sentence: sentence
 	}
 });
 
